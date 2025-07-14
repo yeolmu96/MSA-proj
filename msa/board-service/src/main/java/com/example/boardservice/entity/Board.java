b9effbf853a18d5fa22c0ad7daca754b15a69062
kim-jeongbeom <wjdqja3512@naver.com>
2025-07-14 16:39:41 +0900

    feat: [김정범] 추천 API 구현 [MSA-38]

--- msa/board-service/src/main/java/com/example/boardservice/entity/Board.java
+++ msa/board-service/src/main/java/com/example/boardservice/entity/Board.java
@@ -1,21 +1,23 @@
 package com.example.boardservice.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Entity
 @Getter
 @Setter
-@ToString
+@ToString(exclude = "posts")
 public class Board {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<Post> posts;
 }
--- msa/board-service/src/main/java/com/example/boardservice/entity/Post.java
+++ msa/board-service/src/main/java/com/example/boardservice/entity/Post.java
@@ -8,28 +8,34 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long view_count;
-
     private String title;
 
     private String content;
 
+    @Column(name = "view_count")
+    private int viewCount = 0;
+
+    @Column(name = "recommend_count")
+    private int recommendCount = 0;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
 
-    public Post(String title, String content, Board board) {
+    public Post() {
+    }
+
+    public Post(String title, String content, int viewCount, int recommendCount, Board board) {
         this.title = title;
         this.content = content;
+        this.viewCount = viewCount;
+        this.recommendCount = recommendCount;
         this.board = board;
-        this.view_count = 0L;
     }
 }
