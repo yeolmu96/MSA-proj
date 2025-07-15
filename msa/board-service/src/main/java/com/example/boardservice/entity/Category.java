6109a8521d3edab79c831511a50f8b6c588146fb
kim-jeongbeom <wjdqja3512@naver.com>
2025-07-15 10:44:00 +0900

    feat: [김정범] update, delete 기능 추가 [MSA-51]

--- msa/board-service/src/main/java/com/example/boardservice/entity/Category.java
+++ msa/board-service/src/main/java/com/example/boardservice/entity/Category.java
@@ -1,9 +1,22 @@
 package com.example.boardservice.entity;
 
+import lombok.Getter;
+
+@Getter
 public enum Category {
-    FREE,
-    NOTICE,
-    QNA,
-    JOB,
-    LIFE
+    FREE("자유게시판"),
+    COURSE("훈련과정"),
+    LIFE("일상공유"),
+    MIND("정신치료·심리상담"),
+    EMPLOYMENT("채용해요"),
+    COMPANYLIFE("회사생활"),
+    INTEREST("취미생활"),
+    TRAVEL("여행"),
+    SPORT("스포츠");
+
+    private final String description;
+
+    Category(String description) {
+        this.description = description;
+    }
 }
