acec4a869d11f8c2887ebf0437bd2e3eaffbb20b
IsKJH <dkdlrh147@gmail.com>
2025-07-15 10:30:13 +0900

    chore: [김지한] @repository 엔터티 추가 [MSA-49]

--- msa/information-service/src/main/java/sp/informationservice/repository/InstitutionRepository.java
+++ msa/information-service/src/main/java/sp/informationservice/repository/InstitutionRepository.java
@@ -3,6 +3,7 @@ package sp.informationservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sp.informationservice.entity.Institution;
 
+@Repository
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
     boolean existsByName(String name);
 }
