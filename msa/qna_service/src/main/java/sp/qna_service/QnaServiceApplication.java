dc59660d52839c9ab36759ac3954768884a206a3
IsKJH <dkdlrh147@gmail.com>
2025-07-15 09:03:55 +0900

    init: [김지한] qna_service project setting [MSA-46]

---
+++ msa/qna_service/src/main/java/sp/qna_service/QnaServiceApplication.java
@@ -0,0 +1,13 @@
+package sp.qna_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QnaServiceApplication.class, args);
+	}
+
+}
